ecbc4cc5968feae3fc4a0d97faf20975e8661e64
Deepu2304 <jaideepsai2304@gmail.com>
2024-02-08 10:47:44 +0530

    unzip_script

--- Data_validation.py
+++ Data_validation.py
@@ -2,15 +2,18 @@ import zipfile
 import os
 
 
+# function to unzip the files
 def unzip_files(zip_file_paths, extracted_path):
     for zip_file in zip_file_paths:
-        with zipfile.ZipFile(zip_file, 'r')as zip_ref:
+        with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+            # using this command we can extracted file with the name same as zip file
             extract_subfolder = os.path.join(extracted_path, os.path.splitext(os.path.basename(zip_file))[0])
             os.makedirs(extract_subfolder, exist_ok=True)
             zip_ref.extractall(extract_subfolder)
 
 
 def main():
+    # list of zip_file paths
     zip_file_paths = ['/Users/jaideepsai/Desktop/DATA-ANALYTICS/Data Source/British Airways.zip',
                       '/Users/jaideepsai/Desktop/DATA-ANALYTICS/Data Source/Entertainment.zip',
                       '/Users/jaideepsai/Desktop/DATA-ANALYTICS/Data Source/Finanace.zip',
@@ -22,7 +25,7 @@ def main():
                       '/Users/jaideepsai/Desktop/DATA-ANALYTICS/Data Source/RealEstate.zip',
                       '/Users/jaideepsai/Desktop/DATA-ANALYTICS/Data Source/Automobile.zip'
                       ]
-
+    # the path for the extracted files to store
     extracted_path = '/Users/jaideepsai/Desktop/DATA-ANALYTICS/Data Source/extracts'
 
     unzip_files(zip_file_paths, extracted_path)
